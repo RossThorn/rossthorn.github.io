012bd930d96935944147af9e1b8ab54448d0f213
Ross Thorn <rthorn@wisc.edu>
2017-10-06 09:40:28 -0500

    Fixed text and added new portfolio pieces
    Still need to create pages for those pieces

--- js/includes.js
+++ js/includes.js
@@ -1,4 +1,14 @@
 $(function(){
-    $("#footer").load("footer.html"); 
-    $("#navbar").load("navbar.html"); 
+    $("#footer").load("footer.html");
+    $("#navbar").load("navbar.html");
 });
+
+
+//scrolling background
+$(function(){
+    var x = 0;
+    setInterval(function(){
+        x-=1;
+        $('body').css('background-position','0 ' + x + 'px');
+    }, 80);
+})
