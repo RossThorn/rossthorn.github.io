e8cf3fe1d652447259ac16f22b9d9cca1f67389b
Ross Thorn <rthorn@wisc.edu>
2018-02-26 08:01:57 -0600

    Overlay div fix
    Overlays in index were appearing briefly on load.

--- js/includes.js
+++ js/includes.js
@@ -1,4 +1,10 @@
 $(function(){
+    var x = document.getElementsByClassName("overlay");
+    for(i=0; i<x.length; i++) {
+    x[i].style.display = 'inline';
+    console.log(x[i].style.display);
+  }
+
     //$("#navbar").load("navbar.html");
     $("#footer").load("footer.html");
 });
