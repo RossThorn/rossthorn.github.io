f1f67609821b1d30b8fbbda6c962ab2f5d54b1ef
Ross Thorn <rthorn@wisc.edu>
2018-02-26 08:06:53 -0600

    Cleaned Up test code

--- js/includes.js
+++ js/includes.js
@@ -2,19 +2,7 @@ $(function(){
     var x = document.getElementsByClassName("overlay");
     for(i=0; i<x.length; i++) {
     x[i].style.display = 'inline';
-    console.log(x[i].style.display);
   }
-
     //$("#navbar").load("navbar.html");
     $("#footer").load("footer.html");
 });
-
-
-// //scrolling background
-// $(function(){
-//     var x = 0;
-//     setInterval(function(){
-//         x-=1;
-//         $('body').css('background-position','0 ' + x + 'px');
-//     }, 200);
-// })
