7a4222e7b941f26a3d21494b77877e74632ad343
Ross Thorn <rthorn@wisc.edu>
2019-12-02 21:55:01 -0600

    Changed some css and layout

--- js/includes.js
+++ js/includes.js
@@ -8,28 +8,31 @@ $(function(){
     x[i].style.display = 'inline';
     }
     var foot = document.getElementById("index-foot");
-    // if statement used to determine size of index footer
-    if (x.length % 3 != 0){
-      if(x.length % 2 != 0){
-        // number of projects is not divisible by 2 or 3.
-        foot.className += " col-md-8 col-sm-6 col-xs-12";
-        foot.style.height = height;
+    if (foot){
+      // if statement used to determine size of index footer
+      if (x.length % 3 != 0){
+        if(x.length % 2 != 0){
+          // number of projects is not divisible by 2 or 3.
+          foot.className += " col-md-8 col-sm-6 col-xs-12";
+          foot.style.height = height;
+        } else {
+          // number of projects is divisible by 2 but not 3.
+          foot.className += " col-md-4 col-xs-12";
+
+        }
       } else {
-        // number of projects is divisible by 2 but not 3.
-        foot.className += " col-md-4 col-xs-12";
+        if(x.length % 2 != 0){
+          // number of projects is divisible by 3 but not 2.
+          foot.className += " col-md-12 col-sm-6 col-xs-12";
 
-      }
-    } else {
-      if(x.length % 2 != 0){
-        // number of projects is divisible by 3 but not 2.
-        foot.className += " col-md-12 col-sm-6 col-xs-12";
+        } else {
+          // number of projects is divisible by 2 and 3.
+          foot.className += " col-xs-12";
 
-      } else {
-        // number of projects is divisible by 2 and 3.
-        foot.className += " col-xs-12";
-
+        }
       }
     }
+
 
     //$("#navbar").load("navbar.html");
     //$("#footer").load("footer.html");
@@ -39,5 +42,8 @@ $( window ).resize(function() {
   var height = $('#height-ref').height();
   height = height + 'px';
   var foot = document.getElementById("index-foot");
-  foot.style.height = height;
+  if (foot){
+
+    foot.style.height = height;
+  }
 });
